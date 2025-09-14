8ca02b77d9602d0d8e88759e521f8803579b4212
SKaur-9T <kaur.si@obmontessori.edu.ph>
2025-09-15 06:56:01 +0800

    main.py

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ business_hours = '7:00 AM - 9:00 PM'# string
 year_established = 2025 # integer
 owner_fname = 'Simrandip'# string
 owner_lname = 'Kaur'# string
-tax_rate = 0.15# floating-point
+tax_rate = 0.15 # floating-point
 has_delivery = True # boolean
 
-product_flavor = ''# string
 product_names = [ 'Super Pretzel','Pretzel Bum','Coffee','Pretzel Crisps','Cheese Dip','Mini Pretzel Box','Stuffed Pretzel','Pretzel Sandwich']  # list
 
 menu_prices = (90.0, 60.0, 100.0, 160.0, 30.0, 120.0, 180.0, 200.0) # tuple
@@ -24,28 +23,28 @@ display(f'Since {year_established}', target='yearestablished') # integer
 display(f'Tax rate : {tax_rate} ₱', target='taxrate')  # float
 
 # Product names and prices
-display(f'{product_flavor} {product_names[0]}', target='Super_Pretzel_product') # string + list
+display(f'{product_names[0]}', target='Super_Pretzel_product') # string + list
 display(f'{menu_prices[0]} ₱', target='Super_Pretzel_price')# tuple
 
-display(f'{product_flavor} {product_names[1]}', target='Pretzel_Bum_product') # string + list
+display(f'{product_names[1]}', target='Pretzel_Bum_product') # string + list
 display(f'{menu_prices[1]} ₱', target='Pretzel_Bum_price')# tuple
 
-display(f'{product_flavor} {product_names[2]}', target='Coffee_product') # string + list
+display(f'{product_names[2]}', target='Coffee_product') # string + list
 display(f'{menu_prices[2]} ₱', target='Coffee_price')# tuple
 
-display(f'{product_flavor} {product_names[3]}', target='Pretzel_Crisps_product') # string + list
+display(f'{product_names[3]}', target='Pretzel_Crisps_product') # string + list
 display(f'{menu_prices[3]} ₱', target='Pretzel_Crisps_price')# tuple
 
-display(f'{product_flavor} {product_names[4]}', target='Cheese_Dip_product') # string + list
+display(f'{product_names[4]}', target='Cheese_Dip_product') # string + list
 display(f'{menu_prices[4]} ₱', target='Cheese_Dip_price')# tuple
 
-display(f'{product_flavor} {product_names[5]}', target='Mini_Pretzel_Box_product') # string + list
+display(f'{product_names[5]}', target='Mini_Pretzel_Box_product') # string + list
 display(f'{menu_prices[5]} ₱', target='Mini_Pretzel_Box_price')# tuple
 
-display(f'{product_flavor} {product_names[6]}', target='Stuffed_Pretzel_product') # string + list
+display(f'{product_names[6]}', target='Stuffed_Pretzel_product') # string + list
 display(f'{menu_prices[6]} ₱', target='Stuffed_Pretzel_price')# tuple
 
-display(f'{product_flavor} {product_names[7]}', target='Pretzel_Sandwich_product') # string + list
+display(f'{product_names[7]}', target='Pretzel_Sandwich_product') # string + list
 display(f'{menu_prices[7]} ₱', target='Pretzel_Sandwich_price')# tuple
 
 # Delivery
@@ -59,3 +58,4 @@ display(f'⚠ Be careful, our products contain: {common_allergens}.', target='co
 
 # Business hours
 display(f'Business Hours: {business_hours}', target='businesshours')
+
